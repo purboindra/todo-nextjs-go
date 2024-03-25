c0f6ba62697fff0efb56ef6baf33a67568c8c93b
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-03-25 21:15:21 +0700

    add button

---
+++ nextjs/components/LoadingButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+
+interface LoadingButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  loading: boolean;
+}
+
+export default function LoadingButton({
+  children,
+  loading,
+  ...props
+}: LoadingButtonProps) {
+  return (
+    <Button {...props} disabled={props.disabled || loading}>
+      <span className="flex flex-row gap-1 items-center justify-center">
+        {loading && <Loader2 size={16} className="animate-spin" />}
+        {children}
+      </span>
+    </Button>
+  );
+}
