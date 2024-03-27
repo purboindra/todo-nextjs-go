098533169ed2ad0afb3886d50aa849c379c6f9ae
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-03-27 19:59:17 +0700

    sign up next

--- nextjs/app/api/auth/action.ts
+++ nextjs/app/api/auth/action.ts
@@ -1,15 +1,25 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
 export async function signUp(formData: FormData) {
-  const value = Object.fromEntries(formData);
-
-  const result = await fetch("http://localhost:8080/signup", {
-    method: "POST",
-    body: JSON.stringify({
-      email: value.email,
-      password: value.password,
-      username: value.username,
-    }),
-  });
-
-  console.log(result.body);
-  console.log(result.status);
+  try {
+    const value = Object.fromEntries(formData);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: value.email,
+        password: value.password,
+        username: value.username,
+      }),
+    });
+    console.log(`RESP SIGN UP: ${res.json()}`);
+    console.log(`RESP: ${res.text()}`);
+    redirect("/");
+  } catch (error: any) {
+    throw error;
+  }
 }
