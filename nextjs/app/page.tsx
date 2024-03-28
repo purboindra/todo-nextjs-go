9d5938a8b63a8927768485eb8d9c30cff4ff9caa
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-03-28 21:32:54 +0700

    add new todo button

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,5 +1,6 @@
 import SearchFormField from "@/components/SearchFormField";
 import TodoItem from "@/components/TodoItem";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -7,8 +8,9 @@ export default function Home() {
       <h1 className="text-neutral-700 font-semibold text-2xl text-center">
         TODO LIST
       </h1>
-      <div className="mt-5">
+      <div className="mt-5 space-y-3">
         <SearchFormField />
+        <Button className="flex">Add Todo</Button>
       </div>
       <div className="px-5 items-center flex flex-col space-y-3">
         <TodoItem />
--- nextjs/components/SearchFormField.tsx
+++ nextjs/components/SearchFormField.tsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import LoadingButton from "./LoadingButton";
 
 export default function SearchFormField() {
   const searchSchema = z.object({
