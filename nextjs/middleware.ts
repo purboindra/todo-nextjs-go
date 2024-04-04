983b51e58400a3b54c30a0e4168ea713270963b0
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-04-04 18:51:29 +0700

    middleware

---
+++ nextjs/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const cookies = request.cookies;
+
+  const token = cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  if (
+    request.nextUrl.pathname.startsWith("/sign-in") ||
+    request.nextUrl.pathname.startsWith("/sign-up")
+  ) {
+    if (token) return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/sign-in", "/sign-up"],
+};
